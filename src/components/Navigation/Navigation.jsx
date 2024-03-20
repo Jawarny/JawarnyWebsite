c541e6610a899cc4dfff168f0984638d6ee01d64
Elias Jawarny <Jawarny@hotmail.fr>
2024-03-20 08:24:23 -0400

    modification

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <>
+      <div className="search">
+        <div className="searchMenu"></div>
+        <div className="searchBox"></div>
+      </div>
+      <div className="navItems"></div>
+    </>
+  );
+}
+
+export default Navigation;
